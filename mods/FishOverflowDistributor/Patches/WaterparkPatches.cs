bcac0b41e23ab8df080cb5462ad45e319621c784
MrPurple6411 <mrpurple6411@gmail.com>
2020-01-11 00:46:46 -0700

    BZ Experimental ....

--- mods/FishOverflowDistributor/Patches/WaterparkPatches.cs
+++ mods/FishOverflowDistributor/Patches/WaterparkPatches.cs
@@ -7,32 +7,41 @@ using UnityEngine;
 
 namespace FishOverflowDistributor.Patches
 {
+#if SUBNAUTICA
     [HarmonyPatch(typeof(WaterPark))]
     [HarmonyPatch("TryBreed")]
     internal class WaterparkPatches
     {
         // ReSharper disable once InconsistentNaming
+        [HarmonyPrefix]
         private static bool Prefix(WaterPark __instance, WaterParkCreature creature)
         {
-            FishOverflowDistributor.Logger.LogTrace("WaterPark.TryBreed(WaterParkCreature) called.");
+            Console.WriteLine("WaterPark.TryBreed(WaterParkCreature) called.");
+#elif BELOWZERO
+    [HarmonyPatch(typeof(WaterPark))]
+    [HarmonyPatch(nameof(WaterPark.GetBreedingPartner))]
+    internal class WaterparkPatches
+    {
+        // ReSharper disable once InconsistentNaming
+        [HarmonyPrefix]
+        public static bool Prefix(WaterPark __instance, WaterParkCreature creature, ref WaterParkCreature __result)
+        {
+            __result = null;
+            Console.WriteLine("WaterPark.GetBreedingPartner(WaterParkCreature) called.");
+#endif
 
             //We only distribute when the WaterPark doesn't have any space left for bred creatures.
             //This is the only place in this method where we allow the original unpatched method to execute, 
             //because the original method is never executed when there is no space left in the WaterPark.
 
-            FishOverflowDistributor.Logger.LogTrace(
+            Console.WriteLine(
                 $"__instance.HasFreeSpace() returns '{__instance.HasFreeSpace()}'.");
 
             if (__instance.HasFreeSpace())
                 return true;
+            
 
-            //using Harmony's Traverse class for reflection. Harmony caches MethodInfo, FieldInfo, etc. for further use and thus increases performance.
-            var items = Traverse
-                        .Create(__instance)
-                        .Field("items")
-                        .GetValue<List<WaterParkItem>>();
-
-            if (items == null)
+            if (__instance.rootWaterPark.items == null)
             {
                 FishOverflowDistributor.Logger.LogError(
                     "FieldInfo or value for field 'items' in class 'WaterParkItem' with type 'List<WaterParkItem>' was null -> Should not happen, investigate!.");
@@ -41,26 +50,26 @@ namespace FishOverflowDistributor.Patches
             }
 
 
-            FishOverflowDistributor.Logger.LogTrace("Checking if creature is contained in items");
+            Console.WriteLine("Checking if creature is contained in items");
 
             //Don't know why this check is needed. Maybe TryBreed gets called on fish which arent contained in this WaterPark instance.
-            if (!items.Contains(creature))
+            if (!__instance.rootWaterPark.items.Contains(creature))
                 return false;
 
             TechType creatureTechType = creature.pickupable.GetTechType();
-            FishOverflowDistributor.Logger.LogTrace($"Creature Tech Type = {creatureTechType.ToString()}.");
+            Console.WriteLine($"Creature Tech Type = {creatureTechType.ToString()}.");
 
-            FishOverflowDistributor.Logger.LogTrace(
+            Console.WriteLine(
                 "Checking whether creatureEggs.containsKey(creatureTechType)");
 #if SUBNAUTICA
             //we don't want to distribute creature eggs
             if (WaterParkCreature.creatureEggs.ContainsKey(creatureTechType))
                 return false;
 #endif
-            FishOverflowDistributor.Logger.LogTrace(
+            Console.WriteLine(
                 $"Waterpark '{__instance.gameObject.name}' contains creature '{creature.gameObject.name}' and has enough space for another one.");
 
-            var secondCreature = items.Find(item =>
+            var secondCreature = __instance.rootWaterPark.items.Find(item =>
                                                 item != creature &&
                                                 item is WaterParkCreature &&
 
@@ -73,7 +82,7 @@ namespace FishOverflowDistributor.Patches
             if (secondCreature == null)
                 return false;
 
-            FishOverflowDistributor.Logger.LogTrace(
+            Console.WriteLine(
                 $"Waterpark contains two creatures '{creature.gameObject.name}' of TechType '{creatureTechType.ToString()}' which can breed with each other.");
 
             BaseBioReactor suitableReactor;
@@ -85,16 +94,13 @@ namespace FishOverflowDistributor.Patches
                                                  .First(
                                                      reactor =>
                                                      {
-                                                         var itemsContainer = Traverse
-                                                                              .Create(reactor)
-                                                                              .Property("container")
-                                                                              .GetValue<ItemsContainer>();
+                                                         ItemsContainer itemsContainer = reactor.container;
 
                                                          if (itemsContainer != null)
                                                              return itemsContainer.HasRoomFor(
                                                                  creature.pickupable);
 
-                                                         FishOverflowDistributor.Logger.LogTrace(
+                                                         Console.WriteLine(
                                                              $"PropertyInfo or value for property 'container' in class 'BaseBioReactor' with type 'ItemsContainer' was null -> Should not happen, investigate!.");
 
                                                          return false;
@@ -106,14 +112,14 @@ namespace FishOverflowDistributor.Patches
             }
             if (suitableReactor == null)
             {
-                FishOverflowDistributor.Logger.LogTrace("Could not find suitable reactor");
+                Console.WriteLine("Could not find suitable reactor");
                 return false;
             }
 
             //Reset breed time of the second creature so it can't be used to immediately breed again.
             secondCreature.ResetBreedTime();
 
-            FishOverflowDistributor.Logger.LogTrace(
+            Console.WriteLine(
                 $"Found suitable reactor '{suitableReactor.gameObject.name}'.");
 
             //Now we create a pickupable from the WaterParkCreature which we can add to the reactor's inventory.
@@ -152,10 +158,7 @@ namespace FishOverflowDistributor.Patches
 
             var itemToAdd = new InventoryItem(pickupable);
 
-            var reactorItemsContainer = Traverse
-                                        .Create(suitableReactor)
-                                        .Property("container")
-                                        .GetValue<ItemsContainer>();
+            ItemsContainer reactorItemsContainer = suitableReactor.container;
 
             if (reactorItemsContainer == null)
             {
